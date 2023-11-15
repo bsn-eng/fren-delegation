691f364eb34fb16d36e6f0f07ee71a690054bf34
nicehl <nicollas@blockswap.xyz>
2023-11-15 12:01:46 -0700

    updated env vars

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -4,27 +4,20 @@ export const supportedChains = [chain.goerli, chain.mainnet]
 
 export const rpcUrls = {
   [chain.mainnet.id]: `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`,
-  [chain.ropsten.id]: `https://ropsten.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`,
-  [chain.rinkeby.id]: `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`,
-  [chain.goerli.id]: `https://goerli.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`,
-  [chain.kovan.id]: `https://kovan.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`
+  [chain.goerli.id]: `https://goerli.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`
 }
 
 export const explorerUrls = {
   [chain.mainnet.id]: `https://etherscan.io`,
-  [chain.ropsten.id]: `https://ropsten.etherscan.io`,
-  [chain.rinkeby.id]: `https://rinkeby.etherscan.io`,
-  [chain.goerli.id]: `https://goerli.etherscan.io`,
-  [chain.kovan.id]: `https://kovan.etherscan.io`
+  [chain.goerli.id]: `https://goerli.etherscan.io`
 }
 
 export const beaconUrls = {
   [chain.mainnet.id]: `https://beaconcha.in`,
-  [chain.ropsten.id]: `https://ropsten.beaconcha.in`,
   [chain.goerli.id]: `https://prater.beaconcha.in`
 }
 
-export const BEACON_NODE_URL = 'https://info-goerli.joinstakehouse.com/'
+export const BEACON_NODE_URL = process.env.REACT_APP_BEACON_NODE_URL
 
 export const frenDelegationBribeVaultAddresses = {
   [chain.mainnet.id]: [''],
